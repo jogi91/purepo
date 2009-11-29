d9d7a36b5cd66d6bb766e09f6ab2a9e46f6f8c8d
Johannes Weinbuch <jweinbuch@gmx.ch>
2009-11-29 12:46:02 +0100

    Initial Commit
    * backupscript erstellt
    * dokumentation fuer das Backupscript geschrieben.
    * TODO begonnen

---
+++ tools/gitbackup.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# wenn das Skript einen Fehler Produziert hat, nicht mehr weiter ausführen.
+if test -f ~/.gitbackup/scriptblock
+	then
+	echo "Um Backups wieder aufzunehmen, fehler Beheben und scriptblock löschen" >> fehlerlog.txt
+	mail -s ".gitbackup failed" root < fehlerlog.txt
+	exit 1
+fi
+
+# Alte Backupdatei temporär umbenennen
+mv ~/.gitbackup/purepo/ ~/.gitbackup/purepo.old/
+
+# aktuellsten Stand klonen, fehler in Datei umleiten, Rückgabewert speichern
+git clone --bare http://wetterstation.kanti-wohlen.ch/purepo 2> fehlerlog.txt
+r=$?
+
+
+# Wenn clone erfolgreich, dann altes backup löschen, sonst fehlermeldung ausgeben
+if [ $r -eq 0 ]
+	then rm -rf ~/.gitbackup/purepo.old/
+	echo "alles Ordnungsgemäss abgelaufen"
+else
+	rm -rf ~/.gitbackup/purepo
+	mv ~/.gitbackup/purepo.old/ ~/.gitbackup/purepo/
+	echo "Fehler beim Clonen Aufgetreten, altest Backup nicht gelöscht" >> fehlerlog.txt
+	touch scriptblock
+
+fi
