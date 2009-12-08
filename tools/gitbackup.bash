72bfac45653eeb42b0958b36fe4307cdef4ea087
Jogi on Debianserver <jweinbuch@gmx.ch>
2009-12-08 10:50:07 +0100

    Aenderung vomvorherigen Commit auch ausgefuehrt, vorher waren nur die Changenotes angepasst

--- tools/gitbackup.bash
+++ tools/gitbackup.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd /home/jogi/.gitbackup/
+touch /home/jogi/.gitbackup/test
 # wenn das Skript einen Fehler Produziert hat, nicht mehr weiter ausführen.
-if test -f ~/.gitbackup/scriptblock
+if test -f /home/jogi/.gitbackup/scriptblock
 	then
 	echo "Um Backups wieder aufzunehmen, fehler Beheben und scriptblock löschen" >> fehlerlog.txt
 	mail -s ".gitbackup failed" root < fehlerlog.txt
@@ -9,21 +11,21 @@ if test -f ~/.gitbackup/scriptblock
 fi
 
 # Alte Backupdatei temporär umbenennen
-mv ~/.gitbackup/purepo/ ~/.gitbackup/purepo.old/
+mv /home/jogi/.gitbackup/purepo/ /home/jogi/.gitbackup/purepo.old/
 
 # aktuellsten Stand klonen, fehler in Datei umleiten, Rückgabewert speichern
-git clone git@github.com:jogi91/purepo.git 2> fehlerlog.txt
+git clone git@github.com:jogi91/purepo.git 2> fehlerlog.txt >> fehlerlog.txt
 r=$?
 
 
 # Wenn clone erfolgreich, dann altes backup löschen, sonst fehlermeldung ausgeben
 if [ $r -eq 0 ]
-	then rm -rf ~/.gitbackup/purepo.old/
-	echo "alles Ordnungsgemäss abgelaufen"
+	then rm -rf /home/jogi/.gitbackup/purepo.old/
 else
-	rm -rf ~/.gitbackup/purepo
-	mv ~/.gitbackup/purepo.old/ ~/.gitbackup/purepo/
+	rm -rf /home/jogi/.gitbackup/purepo
+	mv /home/jogi/.gitbackup/purepo.old/ /home/jogi/.gitbackup/purepo/
 	echo "Fehler beim Clonen Aufgetreten, altest Backup nicht gelöscht" >> fehlerlog.txt
+	date >> fehlerlog.txt
 	touch scriptblock
-
+	/home/jogi/.gitbackup/gitbackup.bash
 fi
