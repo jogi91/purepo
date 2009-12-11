d3e5870243e76baf5860be4a6c11ad5f03c1d3c8
Johannes Weinbuch <jweinbuch@gmx.ch>
2009-12-11 13:28:40 +0100

    * Dummycode in ruby fuer die Verarbeitung der Logs hinzugefuegt * Modul dataprocessing erstellt * Dateien main.rb, dataprocessing.rb erstellt

---
+++ ruby/dataprocessing.rb
@@ -0,0 +1,16 @@
+#Dies ein Modul, das verschiedene Funktionen fuer Logs und Plots enthaelt, kurz für das dataprocessing
+
+module Dataprocessing
+
+def writeLog(wert,log)
+	#hier kommt der Code hin, der Logs schreibt
+	puts wert
+	puts log
+end
+
+def drawPlot(log)
+#Diese Funktion wird den Plot zeichnen.
+#Das kann z. B. über ein Shellscript realisiert werden, das den Ploticus aufruft.
+end
+
+end
