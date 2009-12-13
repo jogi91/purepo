bac244709632549d978e10876071227b84008613
Johannes Weinbuch <jweinbuch@gmx.ch>
2009-12-14 01:07:43 +0800

    Pfad fuer Graph geaendert

--- ruby/logger.rb
+++ ruby/logger.rb
@@ -1,4 +1,4 @@
 log="logdemo"
 outputfile="graph.png"
 
-system "ploticus -prefab chron data=../log/"+log+" header=yes x=1 y=2 datefmt=mm/dd/yyyy  xinc=\"1 day\" mode=line unittype=datetime title=\"temperatures this week\" -o ../log/"+outputfile+" -png"
+system "ploticus -prefab chron data=../log/"+log+" header=yes x=1 y=2 datefmt=mm/dd/yyyy  xinc=\"1 day\" mode=line unittype=datetime title=\"temperatures this week\" -o ../graph/"+outputfile+" -png"
