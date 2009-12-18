fccb7839d9c88cd46428bcde466f847fc8d50ce1
Johannes Weinbuch <jweinbuch@gmx.ch>
2009-12-18 13:19:12 +0100

    Klasse Spannung mit rudimentaerem Code ausgestattet, Logger eingebaut

--- ruby/dataprocessing.rb
+++ ruby/dataprocessing.rb
@@ -9,16 +9,43 @@ def writeLog(wert,log)
 end
 
 def drawPlot(logfile, outputfile, titel)
-	system "ploticus -prefab chron data=../log/"+logfile+" header=yes x=1 y=2 datefmt=mm/dd/yyyy  xinc=\"1 day\" mode=line 	unittype=datetime title=\"#{titel}\" -o ../graph/"+outputfile+" -png"
+	system "ploticus -prefab chron data=../$log/"+logfile+" header=yes x=1 y=2 datefmt=mm/dd/yyyy  xinc=\"1 day\" mode=line 	unittype=datetime title=\"#{titel}\" -o ../graph/"+outputfile+" -png"
 end
 
 class Spannung
 	#Attribute werden festgelegt:
-	attr_accessor :wert, :quelle
+	attr_accessor :zahlwert, :quelle
+	
+	#initialisierung per default als Temperatur
+	def initialize(zahlwert, quelle = "temperatur")
+		@zahlwert = zahlwert
+		@quelle = quelle
+	end
+	
+	def to_wert
+		case @quelle
+			when "temperatur"
+				self.to_temp
+				$log.debug("to_temp aufgerufen")
+			when "humidity"
+				self.to_humid
+				$log.debug("to_humid aufgerufen")
+			else
+				log.debug "Kein fall trifft zu"
+		end
+	end
 	
 	def to_temp
-		@wert*5
 	end
+	def to_humid
+	end
+	
+	def debug
+		puts "Zahlwert: "+zahlwert.to_s
+		puts "quelle: "+quelle
+		#self.to_wert
+	end
+	
 end
 
 end
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,4 +1,21 @@
 require "dataprocessing"
+require "logger"
+
+$log = Logger.new(STDERR)
+
+include Dataprocessing
+
+$log.debug("App launched")
 
 
-drawPlot("temperatur/#{Time.now.year}temperaturlog","temperatur/#{Time.now.year}temperaturlog.png","Temperaturen des Jahres #{Time.now.year}")
+
+# Array mit allen zu verarbeitenden Werten fuellen als Konfiguration:
+werte = ["temperatur", "weiterer Wert"]
+$log.debug("folgende Werte werden ausgegeben")
+$log.debug(werte)
+
+foo = Spannung.new 4
+
+foo.to_wert
+
+#drawPlot("temperatur/#{Time.now.year}temperatur$log","temperatur/#{Time.now.year}temperatur$log.png","Temperaturen des Jahres #{Time.now.year}")
