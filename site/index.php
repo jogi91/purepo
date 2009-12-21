7bacecdddc9dbdbc918173b5ec7862ed67973030
Johannes Weinbuch <jweinbuch@gmx.ch>
2009-12-21 15:38:24 +0100

    Beispielcode hinzugefuegt, um Dateien aus einem Verzeichnis auszulesen (nicht funktional)

--- site/index.php
+++ site/index.php
@@ -2,5 +2,20 @@
 <head>
 <title>Wetterstationsdaten</title>
 </head>
+<body>
+
+<?php
+//Routine, um Dateien aus einem Verzeichnis auszulesen:
+$verzeichnis = openDir("graph");
+while($file = readDir($verzeichnis)){
+	if($file!="." && $file != ".."){
+		echo "<a href=\"$file\">$file</a><br>\n";
+	}
+}
+closeDir($verzeichnis);
+?>
+
+
+</body>
 </html>
 
