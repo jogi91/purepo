60a7f8b8e403ded605ffa942225c57facababe1e
Johannes Weinbuch <jweinbuch@gmx.ch>
2010-01-22 13:38:30 +0100

    Methoden geschrieben, um Verbindung zum Controller zu initialisieren, oeffnen und schliessen

--- ruby/controller.rb
+++ ruby/controller.rb
@@ -3,4 +3,29 @@
 #http://www.zenspider.com/ZSS/Products/RubyInline/
 
 class Controller
+
+	inline do |builder|
+		builder.c ' 
+		void initialize() { //Initialisierung -> octopus init und open
+		struct octopus_context octopus;
+		if (!octopus_init (&octopus))
+			printf ("%s\n", octopus.error_str);
+		if (octopus_open (&octopus) < 0) {
+			printf ("%s\n", octopus.error_str);
+			exit (0);
+		}
+		char desc[64];
+		octopus_get_hwdesc (&octopus, desc);
+		printf ("Device: %s\n", desc);
+		}'
+	end
+	
+	inline do |builder|
+		builder.c '
+		void close() { //close
+		if (octopus_close (&octopus) < 1)
+	 		printf ("ERROR: %s\n", octopus.error_str);
+		}'
+	end
+
 end
