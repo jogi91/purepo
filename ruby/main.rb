777081fc845a9248c79ae5378524d1cc9dd2b582
Johannes Weinbuch <jweinbuch@gmx.ch>
2010-05-06 15:54:52 +0200

    Datenschleifen vereint

--- ruby/main.rb
+++ ruby/main.rb
@@ -21,30 +21,26 @@ $log.info(werte)
 werte.each { |wert|
 	#Spannungsobjekt erstellen
 	zahlwert = getVoltage(wert)
-	@object = Spannung.new(zahlwert, wert)
+	object = Spannung.new(zahlwert, wert)
 	$log.info(wert)
 
 	#Wert in das Jahreslogfile schreiben z.B. log/2009/temperatur/logyearly
-	writeLog(wert.to_i, Time.now.year.to_s+"/"+@object.quelle+"/logyearly")
+	writeLog(wert.to_i, Time.now.year.to_s+"/"+object.quelle+"/logyearly")
 
 	$log.debug("Jahreslog fertig geschrieben")
 
 	#Wert in das Monatslogfile schreiben
-	writeLog(@object.to_i, Time.now.year.to_s+"/"+@object.quelle+"/log"+Time.now.strftime("%B"))
+	writeLog(object.to_i, Time.now.year.to_s+"/"+object.quelle+"/log"+Time.now.strftime("%B"))
 
 	$log.debug("Monatslog fertig geschrieben")
 
-	}
-
-#erst wenn alle Logs geschrieben sind, die Graphen zeichnen.
-werte.each {|wert|
 	#Jahreslog plotten
-	drawPlot(Time.now.year.to_s+"/"+wert+"/logyearly", wert+"/#{Time.now.year}.png", @object.diagrammtiteljahr)
+	drawPlot(Time.now.year.to_s+"/"+wert+"/logyearly", wert+"/#{Time.now.year}.png", object.diagrammtiteljahr)
 
 	$log.debug("Jahreslog fertig geplottet")
 
 	#Monatslog plotten
-	drawPlot(Time.now.year.to_s+"/"+wert+"/log"+Time.now.strftime("%B"), wert+"/#{Time.now.strftime('%m')}-#{Time.now.year}.png", @object.diagrammtitelmonat)
+	drawPlot(Time.now.year.to_s+"/"+wert+"/log"+Time.now.strftime("%B"), wert+"/#{Time.now.strftime('%m')}-#{Time.now.year}.png", object.diagrammtitelmonat)
 	$log.debug("Jahreslog fertig geplottet")
 
 }
